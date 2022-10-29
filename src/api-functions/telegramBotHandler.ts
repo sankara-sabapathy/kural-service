ba0ce0c8e3bcaf86f773e283ec2a734e412deff6
sankara sabapathy <krssabapathy1999@gmail.com>
2022-10-29 05:20:05 +0000

    telegram bot handler fix.

--- src/api-functions/telegramBotHandler.ts
+++ src/api-functions/telegramBotHandler.ts
@@ -11,36 +11,45 @@ app.use(bodyParser.json({ strict: false }));
 app.use(cors());
 module.exports.handler = serverless(app);
 app.post("/botHandler", async (req, res) => {
-    console.log("Received update. Thanks Telegram");
-    res.status(200).json({ "Message": "Received update. Thanks Telegram" }); // To avoid retries from telegram
-    const chatId = req.body.message.chat.id;
-    const graphqlService: GraphQLService = new GraphQLService();
-    const secretManagement: SecretManagement = new SecretManagement();
-    let secretValue;
-    if (!CredentialsService.getHasuraAdminSecret()) {
-        secretValue = await secretManagement.getSecret();
-    }
-    if (!secretValue) {
-        console.log("Failed to get Secrets from Secret manager", secretValue);
-        return;
-    }
-    CredentialsService.setSecretValue(secretValue.xHasuraAdminSecret, secretValue.sibApiKey);
-    if (req.body.message.text === '/random') {
-        const getKuralForEmailResult = await graphqlService.getKuralForEmail(randomNumber());
-        if (!getKuralForEmailResult || getKuralForEmailResult.thirukkural.length < 1) {
-            console.log("Failed to get Thirukkural from DB", getKuralForEmailResult);
+    try {
+        const chatId = req.body.message.chat.id;
+        const graphqlService: GraphQLService = new GraphQLService();
+        const secretManagement: SecretManagement = new SecretManagement();
+        let secretValue;
+        if (!CredentialsService.getHasuraAdminSecret()) {
+            secretValue = await secretManagement.getSecret();
+        }
+        if (!secretValue) {
+            console.log("Failed to get Secrets from Secret manager", secretValue);
+            res.status(200).json({ "Message": "Failed to get Secrets from Secret manager" }); // Always set statuscode to 200, to avoid retries from telegram.
             return;
         }
-        try {
-            const uri: any = await getFormattedURL(getKuralForEmailResult, secretValue.botToken, chatId);
-            axios.get(encodeURI(uri));
-            return;
-        } catch (err) {
-            console.log('Error while sending kural using telegram bot: ' + err);
+        CredentialsService.setSecretValue(secretValue.xHasuraAdminSecret, secretValue.sibApiKey);
+        if (req.body.message.text === '/random') {
+            const getKuralForEmailResult = await graphqlService.getKuralForEmail(randomNumber());
+            if (!getKuralForEmailResult || getKuralForEmailResult.thirukkural.length < 1) {
+                console.log("Failed to get Thirukkural from DB", getKuralForEmailResult);
+                res.status(200).json({ "Message": "Failed to get Thirukkural from DB" }); // Always set statuscode to 200, to avoid retries from telegram.
+                return;
+            }
+            try {
+                const uri: any = await getFormattedURL(getKuralForEmailResult, secretValue.botToken, chatId);
+                await axios.get(encodeURI(uri));
+                res.status(200).json({ "Message": "Success" }); // Always set statuscode to 200, to avoid retries from telegram.
+                return;
+            } catch (err) {
+                console.log('Error while sending kural using telegram bot: ' + err);
+                res.status(200).json({ "Message": "Error while sending kural using telegram bot" }); // Always set statuscode to 200, to avoid retries from telegram.
+                return;
+            }
+        } else {
+            await axios.get(encodeURI('https://api.telegram.org/bot' + secretValue.botToken + '/sendMessage?chat_id=' + chatId + '&text=' + 'I am only a kid under development :-) Get a random Thirukkural from menu.'));
+            res.status(200).json({ "Message": "Invalid request" }); // Always set statuscode to 200, to avoid retries from telegram.
             return;
         }
-    } else {
-        axios.get(encodeURI('https://api.telegram.org/bot' + secretValue.botToken + '/sendMessage?chat_id=' + chatId + '&text=' + 'I am a kid under development :-) Get a random Thirukkural from menu.'));
+    } catch (error) {
+        console.log("Internal server error", error);
+        res.status(200).json({ "Message": "Success" }); // Always set statuscode to 200, to avoid retries from telegram.
         return;
     }
 })
